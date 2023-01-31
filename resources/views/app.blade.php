f978d799b26daf33edcd6800495ad6c72f3a2d99
Fernando <shaaf4ever@gmail.com>
2023-01-31 11:34:58 -0600

    Install inertia server-side

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ env('APP_NAME') }}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @vite('resources/js/app.js')
+    @inertiaHead
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -14,10 +14,7 @@
         }
     </style>
 </head>
-<body class="antialiased">
-<div id="app">
-    Hello.
-</div>
+<body>
+@inertia
 </body>
 </html>
-
